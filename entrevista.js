605ab9810ec9e9373cab48497539e6a38e281a0d
Leo <herreraleonardo061@gmail.com>
2025-06-03 12:47:27 -0300

    Añadido funcion de JS

---
+++ entrevista.js
@@ -0,0 +1,16 @@
+let txtButton = document.getElementById("btn")
+let txtNombre = document.getElementById("nya2")
+let txtContact = document.getElementById("cnt")
+let txtDni = document.getElementById("dni")
+let txtSede = document.getElementById("sede")
+let txtInfo = document.getElementById("info")
+let a = "Enviado"
+
+let nombre = ""
+
+txtButton.addEventListener("click", () => {
+    alert(a)
+    console.log(a)
+    txtNombre = nombre
+    console.log(txtNombre)
+})
