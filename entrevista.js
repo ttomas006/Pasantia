10f01e81d1c479b165823438a580449a9665ccec
Leo <herreraleonardo061@gmail.com>
2025-06-05 12:16:06 -0300

    Actualizado de JS y añadido de seccion

--- entrevista.js
+++ entrevista.js
@@ -1,16 +1,34 @@
 let txtButton = document.getElementById("btn")
-let txtNombre = document.getElementById("nya2")
-let txtContact = document.getElementById("cnt")
-let txtDni = document.getElementById("dni")
-let txtSede = document.getElementById("sede")
-let txtInfo = document.getElementById("info")
+let txtName = document.getElementById("name-form")
+let txtAdress = document.getElementById("adress-form")
+let txtEmail = document.getElementById("email-form")
+let txtTel = document.getElementById("tel-form")
+let txtDni = document.getElementById("dni-form")
+let txtSede = document.getElementById("sede-form")
+let txtComentario = document.getElementById("info-form")
 let a = "Enviado"
 
-let nombre = ""
+
 
 txtButton.addEventListener("click", () => {
     alert(a)
-    console.log(a)
-    txtNombre = nombre
-    console.log(txtNombre)
+
+    const nombre = txtName.value
+    const apellido = txtAdress.value
+    console.log("Nombre: " + nombre + "|" + "Apellido: " + apellido)
+
+    const email = txtEmail.value
+    const tel = txtTel.value
+    console.log("Email: " + email + "|" + "Telefono: " + tel)
+
+    const dni = txtDni.value
+    console.log("DNI: " + dni)
+
+    const sede = txtSede.value
+    console.log("Sede: " + sede)
+
+    const comentario = txtComentario.value
+    console.log("Comentario: " + comentario)
+
 })
+
