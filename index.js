66bcc1ce5ba84e568336dc50c2d19a32a62e96e6
Leo <herreraleonardo061@gmail.com>
2025-06-12 11:55:45 -0300

    Actualizacion de Header

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
         const menuBtn = document.querySelector('.menu-btn');
         const menuItems = document.querySelector('.menu-items');
 
+        menuItems.style.display = "none"
+
         menuBtn.addEventListener('click', () => {
-            menuItems.style.display = (menuItems.style.display === 'block') ? 'none' : 'block';
+            menuItems.style.display = (menuItems.style.display === 'flex') ? 'none' : 'flex';
         });
+
+        const menuBtnX = document.querySelector('.menu-btn-2')
+
+        menuBtnX.addEventListener('click', () => {
+            menuItems.style.display = "none"
+        })
