94152e9f49bb12ec5b4a07949abebd9e086f900d
Leo <herreraleonardo061@gmail.com>
2025-07-01 11:55:57 -0300

    Login finalizado

--- entrevista.php
+++ entrevista.php
@@ -2,33 +2,8 @@
 
 include "configdb.php";
 
-if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
-}
+include "registrodb.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST["nombre"];
-    $apellido = $_POST["apellido"];
-    $email = $_POST["email"];
-    $numero = $_POST["numero"];
-    $dni = $_POST["dni"];
-    $sede = $_POST["sede"];
-    $comentario = $_POST["comentario"];
-
-    $sql = "INSERT INTO entrevista (nombre, apellido, email, numero, dni, sede, comentario)
-            VALUES (?, ?, ?, ?, ?, ?, ?)";
-
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("sssssss", $nombre, $apellido, $email, $numero, $dni, $sede, $comentario);
-
-    if ($stmt->execute()) {
-        echo "<p style='color: green;'>¡Datos guardados exitosamente!</p>";
-    } else {
-        echo "<p style='color: red;'>Error: " . $stmt->error . "</p>";
-    }
-
-    $stmt->close();
-}
 ?>
 
 <!DOCTYPE html>
